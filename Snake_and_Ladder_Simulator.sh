922f1e62a5ed67386b8f90d7e46e7b47664e2301
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-09 14:27:25 +0530

    Introduced Number of players and starting position

--- Snake_and_Ladder_Simulator.sh
+++ Snake_and_Ladder_Simulator.sh
@@ -1,1 +1,8 @@
+#!/bin/bash -x
+
 echo "Welcome to Snake and Ladder Simulation Program"
+
+PLAYER=1
+START_POSITION=0
+
+echo "Number of players are : $PLAYER and Starting position is $START_POSITION"
