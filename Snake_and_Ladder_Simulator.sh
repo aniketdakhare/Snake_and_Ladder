e25a0cf8fb8cd64ef2f5d936318f0c4da14ab40a
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-09 15:00:45 +0530

    No_Play, Ladder and Snake condition

--- Snake_and_Ladder_Simulator.sh
+++ Snake_and_Ladder_Simulator.sh
@@ -4,12 +4,28 @@ echo "Welcome to Snake and Ladder Simulation Program"
 
 PLAYER=1
 START_POSITION=0
+NOPLAY=0
+LADDER=1
+SNAKE=2
+new_position=$START_POSITION
 
-function dice()
+function movement()
 {
 	dice_Value=$(($((RANDOM%6))+1))
+	move=$((RANDOM%3))
+	case $move in
+		$NOPLAY)
+			new_position+=0
+			;;
+		$LADDER)
+			new_position=$(($new_position+$dice_Value))
+			;;
+		$SNAKE)
+			new_position=$(($new_position - $dice_Value))
+			;;
+	esac
 }
 
-dice
+movement
 
-echo "Dice value is : $dice_Value"
+echo "Current position of player is at $new_position place"
